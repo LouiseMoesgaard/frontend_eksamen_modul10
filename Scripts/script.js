2ab18d8a6e72ae03d2fb20c8451867fd0581fafb
Louise Moesgaard Nielsen <louise@moesgaard.dk>
2020-05-24 10:24:29 +0200

    6. iteration af nav, footer er done og scroll up btn er opsat

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,5 @@
+getPage();
 document.addEventListener("DOMContentLoaded", function () {
-    getPage();
     const nav = document.querySelector("nav .menu");
 
     document.querySelector(".burger_open").addEventListener("click", function () {
@@ -8,4 +8,25 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".burger_close").addEventListener("click", function () {
         nav.classList.add("hide")
     })
+
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        scrollFunction()
+    };
+
+    function scrollFunction() {
+        let scrollBtn = document.querySelector("#scroll_btn");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollBtn.classList.remove("hide");
+        } else {
+            scrollBtn.classList.add("hide");
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
 });
