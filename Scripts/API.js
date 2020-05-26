8a0223456af5c5277ae6b890b615ae81fa4e712d
niko702j <niko702j@stud.kea.dk>
2020-05-26 12:52:33 +0200

    rettelser

--- Scripts/API.js
+++ Scripts/API.js
@@ -3,36 +3,50 @@ const endpoint = "http://moesgaard-designs.dk/kea/10-eksamen/wordpress/wp-json/w
 const frontPageOrder = ["landing-page", "about", "videos", "photos", "reviews"];
 
 async function getPage() {
-    const urlParams = new URLSearchParams(window.location.search);
-    let id = urlParams.get("id");
-    if (!id) {
-        id = 1;
-    }
-    if (id !== 1) {
-        const response = await fetch(endpoint + "posts/" + id);
-        let post = await response.json();
+  const urlParams = new URLSearchParams(window.location.search);
+  let id = urlParams.get("id");
+  if (!id) {
+    id = 1;
+  }
+  if (id !== 1) {
+    const response = await fetch(endpoint + "posts/" + id);
+    let post = await response.json();
 
-        var main = document.querySelector("main");
-        main.innerHTML = post.content.rendered;
-    } else {
-        const response = await fetch(endpoint + "posts?categories=2");
-        let posts = await response.json();
-        let result = [];
-        frontPageOrder.forEach(key => {
-            let found = false;
-            posts.filter(post => {
-                if (!found && post.slug === key) {
-                    result.push(post);
-                    found = true;
-                    return false;
-                } else {
-                    return true;
-                }
-            })
-        });
+    var main = document.querySelector("main");
+    main.innerHTML = post.content.rendered;
+  } else {
+    const response = await fetch(endpoint + "posts?categories=2");
+    let posts = await response.json();
+    let result = [];
+    frontPageOrder.forEach(key => {
+      let found = false;
+      posts.filter(post => {
+        if (!found && post.slug === key) {
+          result.push(post);
+          found = true;
+          return false;
+        } else {
+          return true;
+        }
+      })
+    });
 
-        var main = document.querySelector("main");
-        main.classList.add("frontpage");
-        main.innerHTML = result.map(post => post.content.rendered).join();
-    }
+    var main = document.querySelector("main");
+    main.classList.add("frontpage");
+    main.innerHTML = result.map(post => post.content.rendered).join();
+  }
+}
+
+async function getGallery() {
+  const response = await fetch(endpoint + "gallery/" + id)
+  let gallery = await response.json();
+
+  visGallery(json);
+}
+
+async function getLyrics() {
+  const response = await fetch(endpoint + "lyric")
+  let gallery = await response.json();
+
+  visLyrics(json);
 }
--- Scripts/script.js
+++ Scripts/script.js
@@ -1,31 +1,85 @@
 getPage();
 document.addEventListener("DOMContentLoaded", function () {
-    const nav = document.querySelector("nav .menu");
+  const nav = document.querySelector("nav .menu");
 
-    document.querySelector(".burger_open").addEventListener("click", function () {
-        nav.classList.toggle("hide")
-        this.classList.toggle("open_menu")
-    })
+  document.querySelector(".burger_open").addEventListener("click", function () {
+    nav.classList.toggle("hide")
+    this.classList.toggle("open_menu")
+  })
 
 
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-        scrollFunction()
-    };
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction()
+  };
 
-    function scrollFunction() {
-        let scrollBtn = document.querySelector("#scroll_btn");
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            scrollBtn.classList.remove("hide");
-        } else {
-            scrollBtn.classList.add("hide");
-        }
+  function scrollFunction() {
+    let scrollBtn = document.querySelector("#scroll_btn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollBtn.classList.remove("hide");
+      scrollBtn.addEventListener("click", topFunction);
+    } else {
+      scrollBtn.classList.add("hide");
+      scrollBtn.removeEventListener("click", topFunction);
     }
+  }
 
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    window.scrollTo(0, 0);
+  }
 
 });
+
+
+function visGallery(images) {
+  var galleryDOM = document.querySelector(".slider");
+
+  if (!galleryDOM) {
+    console.error("Trying to add images without DOM object present");
+    return;
+  } else {
+    if (id = 73) {
+      const template = document.querySelector(".sli_dj").content;
+      images.forEach(image => {
+        const klon = template.cloneNode(true);
+        klon.querySelector("img").src = image.guid;
+        galleryDOM.appendChild(klon);
+      });
+    }
+    if (id = 78) {
+      const template = document.querySelector(".sli_c").content;
+      images.forEach(image => {
+        const klon = template.cloneNode(true);
+        klon.querySelector("img").src = image.guid;
+        galleryDOM.appendChild(klon);
+      });
+    }
+    if (id = 84) {
+      const template = document.querySelector(".sli_k").content;
+      images.forEach(image => {
+        const klon = template.cloneNode(true);
+        klon.querySelector("img").src = image.guid;
+        galleryDOM.appendChild(klon);
+      });
+    }
+  }
+}
+
+function visLyrics(lyrics) {
+  console.log("vis lyrics");
+  var lyricsDOM = document.querySelector(".lyricswrapper");
+  if (!lyricsDOM) {
+    console.error("Trying to add lyrics without DOM object present");
+    return;
+  } else {
+    const template = document.querySelector(".lyric_collapse");
+    texts.forEach(lyric => {
+      const klon = template.cloneNode(true);
+      klon.querySelector(".songtitle").textcontent = lyric.song_title;
+      klon.querySelector(".original").textContent = lyric.lyrics_original;
+      klon.querySelector(".english").textContent = lyric.lyrics_english;
+      lyricsDOM.appendChild(klon);
+    });
+  }
+}
