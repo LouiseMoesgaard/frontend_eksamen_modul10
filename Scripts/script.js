1c4b0d8b67db32b5bc24373347a85451c910d3bd
Louise Moesgaard Nielsen <louise@moesgaard.dk>
2020-05-26 15:29:53 +0200

    laver colapse btn

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,85 +1,68 @@
 getPage();
+scrollFunction();
+
 document.addEventListener("DOMContentLoaded", function () {
-  const nav = document.querySelector("nav .menu");
+    const nav = document.querySelector("nav .menu");
 
-  document.querySelector(".burger_open").addEventListener("click", function () {
-    nav.classList.toggle("hide")
-    this.classList.toggle("open_menu")
-  })
+    document.querySelector(".burger_open").addEventListener("click", function () {
+        nav.classList.toggle("hide")
+        this.classList.toggle("open_menu")
+    })
 
-
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function () {
-    scrollFunction()
-  };
-
-  function scrollFunction() {
-    let scrollBtn = document.querySelector("#scroll_btn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      scrollBtn.classList.remove("hide");
-      scrollBtn.addEventListener("click", topFunction);
-    } else {
-      scrollBtn.classList.add("hide");
-      scrollBtn.removeEventListener("click", topFunction);
-    }
-  }
-
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    window.scrollTo(0, 0);
-  }
-
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        scrollFunction()
+    };
 });
 
+function scrollFunction() {
+        let scrollBtn = document.querySelector("#scroll_btn");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollBtn.classList.remove("hide");
+            scrollBtn.addEventListener("click", topFunction);
+        } else {
+            scrollBtn.classList.add("hide");
+            scrollBtn.removeEventListener("click", topFunction);
+        }}
 
-function visGallery(images) {
-  var galleryDOM = document.querySelector(".slider");
+  // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        window.scrollTo(0, 0);
+    }
 
-  if (!galleryDOM) {
-    console.error("Trying to add images without DOM object present");
-    return;
-  } else {
-    if (id = 73) {
-      const template = document.querySelector(".sli_dj").content;
-      images.forEach(image => {
-        const klon = template.cloneNode(true);
-        klon.querySelector("img").src = image.guid;
-        galleryDOM.appendChild(klon);
-      });
+
+function buildLyrics() {
+    let lyricDOM = document.querySelector(".lyrics");
+    let template = document.querySelector("template#collapse")
+    if(template){
+        getLyrics().then(lyrics=>{
+            lyrics.forEach(lyric => {
+                let collapse = template.content.cloneNode(true);
+                collapse.querySelector(".title").innerHTML = lyric.title.rendered;
+                collapse.querySelector("p").innerHTML = lyric.lyrics_original;
+
+                lyricDOM.appendChild(collapse);
+            })
+            document.querySelectorAll(".collapse").forEach(collapse=>{
+                collapse.addEventListener("click", function () {
+                    this.classList.toggle("open");
+                    let icon = this.querySelector(".arrow");
+                    if (icon.classList.contains("open")) {
+                        icon.classList.remove("open");
+                    } else {
+                        icon.classList.add("open");
+                    }
+                })
+            })
+        });
     }
-    if (id = 78) {
-      const template = document.querySelector(".sli_c").content;
-      images.forEach(image => {
-        const klon = template.cloneNode(true);
-        klon.querySelector("img").src = image.guid;
-        galleryDOM.appendChild(klon);
-      });
-    }
-    if (id = 84) {
-      const template = document.querySelector(".sli_k").content;
-      images.forEach(image => {
-        const klon = template.cloneNode(true);
-        klon.querySelector("img").src = image.guid;
-        galleryDOM.appendChild(klon);
-      });
-    }
-  }
+
+
 }
 
-function visLyrics(lyrics) {
-  console.log("vis lyrics");
-  var lyricsDOM = document.querySelector(".lyricswrapper");
-  if (!lyricsDOM) {
-    console.error("Trying to add lyrics without DOM object present");
-    return;
-  } else {
-    const template = document.querySelector(".lyric_collapse");
-    texts.forEach(lyric => {
-      const klon = template.cloneNode(true);
-      klon.querySelector(".songtitle").textcontent = lyric.song_title;
-      klon.querySelector(".original").textContent = lyric.lyrics_original;
-      klon.querySelector(".english").textContent = lyric.lyrics_english;
-      lyricsDOM.appendChild(klon);
-    });
-  }
+function APIHook(){
+    buildLyrics();
 }
+
+
+
